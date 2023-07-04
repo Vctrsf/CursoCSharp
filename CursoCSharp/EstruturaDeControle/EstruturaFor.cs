c77dfe4824eb52e5418f995b9d23cb802ed0eeac
Victor <btf.victorsf0206@gmail.com>
2023-07-04 15:47:49 -0300

    Adição das classes "EstruturaBreak", "EstruturaFor", "EstruturaForEach" e "UsandoContinue"

--- CursoCSharp/EstruturaDeControle/EstruturaFor.cs
+++ CursoCSharp/EstruturaDeControle/EstruturaFor.cs
@@ -13,10 +13,28 @@ namespace CursoCSharp.EstruturaDeControle
             //for(int i = 1; i <= 10; i++)
             //{
             //    Console.WriteLine($"O valor de i é {i}.");
-                
-            //}
-            
 
+            //}
+
+            double somatorio = 0;
+            string entrada;
+
+            Console.Write("Informe o tamanho da turma: ");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int tamanhoTurma);
+
+            for (int i = 1; i <= tamanhoTurma; i++)
+            {
+                Console.WriteLine("Informe a nota do aluno {0}", i);       
+                entrada = Console.ReadLine();
+
+                double.TryParse(entrada, out double notaAtual); 
+
+                somatorio += notaAtual;
+             }
+
+            double media = tamanhoTurma > 0 ? somatorio / tamanhoTurma : 0;
+            Console.WriteLine("Média da turma: {0}", media);
         }
     }
 }
