3295de0a278bd54c67ebde1c5018d7cf4d832592
Victor <btf.victorsf0206@gmail.com>
2023-07-04 11:46:45 -0300

    Adição das classes "EstruturaDoWhile" e "EstruturaFor"

---
+++ CursoCSharp/EstruturaDeControle/EstruturaFor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    internal class EstruturaFor
+    {
+        public static void Executar()
+        {
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"O valor de i é {i}.");
+                
+            //}
+            
+
+        }
+    }
+}
