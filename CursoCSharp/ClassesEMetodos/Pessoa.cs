2e2eaf4a2689f5f54e2cd655fd14fab49fc0fb00
Victor <btf.victorsf0206@gmail.com>
2023-07-06 19:49:43 -0300

    Adição das classes "Membro" e "Pessoa"

---
+++ CursoCSharp/ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    internal class Pessoa
+    {
+        /*public - Torna o atributo visível para fora da classe
+          string tem valor padrão null        
+          Int tem valor padrão 0
+         */
+        public string Nome;
+        public int Idade;
+
+        public string Apresentar()
+        {
+            return string.Format(
+                $"Olá! me chamo {Nome} e tenho {Idade} anos.");
+        }
+        
+        public void ApresentarNoConsole()
+        {
+            Console.WriteLine(Apresentar());
+        }
+         
+    }
+}
