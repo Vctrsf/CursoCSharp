4da70f7f8870d663cb8c40be5add8773a3cc1c44
Victor <btf.victorsf0206@gmail.com>
2023-08-14 16:29:38 -0300

    Adição do método "ColecoesList"

--- CursoCSharp/Colecoes/ColecoesList.cs
+++ CursoCSharp/Colecoes/ColecoesList.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,38 +12,54 @@ namespace CursoCSharp.Colecoes
         public string Nome;
         public double Preco;
 
-            public Produto(string nome, double preco)
+        public Produto(string nome, double preco)
         {
             Nome = nome;
             Preco = preco;
+
         }
-    }
-    internal class ColecoesList
-    {
-        public static void Executar()
+        public override bool Equals(object obj)
         {
-            var livro = new Produto("Game of Thrones", 49.9);
+            Produto outroProduto = (Produto)obj;
+            bool mesmoNome = Nome == outroProduto.Nome;
+            bool mesmoPreco = Preco == outroProduto.Preco;
+            return mesmoNome && mesmoPreco;
+        }
 
-            var carrinho = new List<Produto>();
-            carrinho.Add(livro);
+        public override int GetHashCode()
+        {
+            var hashCode = -347481536;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + Preco.GetHashCode();
+            return hashCode;
+        }
+        internal class ColecoesList
+        {
+            public static void Executar()
+            {
+                var livro = new Produto("Game of Thrones", 49.9);
 
-            var combo = new List<Produto>
+                var carrinho = new List<Produto>();
+                carrinho.Add(livro);
+
+                var combo = new List<Produto>
             {
                 new Produto("Camisa", 29.9),
                 new Produto("8° Temporada Game of Thrones", 99.9),
                 new Produto("Poster", 10)
             };
 
-            carrinho.AddRange(combo);
-            Console.WriteLine(carrinho.Count);
-            carrinho.RemoveAt(3);
+                carrinho.AddRange(combo);
+                Console.WriteLine(carrinho.Count);
+                carrinho.RemoveAt(3);
 
-            foreach (var item in carrinho)
-            {
-                Console.Write(carrinho.IndexOf(item));
-                Console.WriteLine($" {item.Nome} {item.Preco}");
+                foreach (var item in carrinho)
+                {
+                    Console.Write(carrinho.IndexOf(item));
+                    Console.WriteLine($" {item.Nome} {item.Preco}");
+                }
+
             }
-            
         }
     }
 }
