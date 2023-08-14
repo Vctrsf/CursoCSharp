ea8f614de8f4c620d5aca0a4bb9dabc8bc516ace
Victor <btf.victorsf0206@gmail.com>
2023-08-14 16:31:15 -0300

    Adição dos métodos ''Executar'' das classes "ColecoesQueue", "Igualdade", "ColecoesStack", "ColecoesDictionary"

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -66,9 +66,13 @@ namespace CursoCSharp
 
                 // Coleções
                 {"Array - Coleções", Colecoes.Array.Executar},
-                {"List - Coleções", ColecoesList.Executar},
+                {"List - Coleções", Colecoes.Produto.ColecoesList.Executar},
                 {"Array List - Coleções", ColecoesArrayList.Executar},
                 {"Set - Coleções", ColecoesSet.Executar},
+                {"Queue - Coleções", ColecoesQueue.Executar},
+                {"Igualdade - Coleções", Igualdade.Executar},
+                {"Stack - Coleções", ColecoesStack.Executar},
+                {"Dictionary - Coleções", ColecoesDictionary.Executar},
 
             }); ;
             central.SelecionarEExecutar();
