ab971e7f779760c7bda8641a6ed051d59cbc4d00
Victor <btf.victorsf0206@gmail.com>
2023-08-14 16:29:51 -0300

    Adição do método "ColecoesQueue"

---
+++ CursoCSharp/Colecoes/ColecoesQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes
+{
+    internal class ColecoesQueue
+    {
+        public static void Executar()
+        {
+            var fila = new Queue<string>();
+
+            fila.Enqueue("Fulano");
+            fila.Enqueue("Sicrano");
+            fila.Enqueue("Beltrano");
+
+            Console.WriteLine(fila.Peek()); //Pega um item da fila sem remover
+            Console.WriteLine(fila.Count); //Conta os itens da fila
+
+            Console.WriteLine(fila.Dequeue());
+            Console.WriteLine(fila.Count);
+
+            foreach(var pessoa in fila)
+            {
+                Console.WriteLine(pessoa);
+            }
+            //Fila com vários valores e de estrutura dinâmica
+            var salada = new Queue();
+            salada.Enqueue(3);
+            salada.Enqueue("Item");
+            salada.Enqueue(true);
+            salada.Enqueue(3.14);
+
+            Console.WriteLine(salada.Contains("item"));
+            Console.WriteLine(salada.Contains("item"));
+        }
+
+    }
+}
