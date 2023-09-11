622e3f57dbfe5b7472a6deee686f5b0826f92a7f
Victor <btf.victorsf0206@gmail.com>
2023-09-11 15:56:44 -0300

    Adição da classe "LambdasDelegate"

---
+++ CursoCSharp/MetodosEFuncoes/LambdasDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.MetodosEFuncoes
+{
+    //Delegate é uma função que requer uma "assinatura";
+    //Assinatura seriam os tipos de parametros e o tipo de retorno dessa função
+    delegate double Operacao(double x, double y);
+    class LambdasDelegate
+    {
+        public static void Executar()
+        {
+            Operacao sum = (x, y) => x + y;
+            Operacao sub = (x, y) => x - y;
+            Operacao mult = (x, y) => x * y;
+
+            Console.WriteLine(sum(3, 3));
+            Console.WriteLine(sub(5, 2));
+            Console.WriteLine(mult(12, 8));
+
+            
+        }
+    }
+}
