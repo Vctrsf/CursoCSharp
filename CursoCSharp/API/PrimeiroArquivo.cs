738dc2d5d5e9da3bc364c84a9dd623beb9b47714
Victor <btf.victorsf0206@gmail.com>
2023-09-12 16:47:11 -0300

    Adição da classe "PrimeiroArquivo"

---
+++ CursoCSharp/API/PrimeiroArquivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace CursoCSharp.API
+{
+    public static class ExtensaoString
+    {
+        public static string ParseHome(this string path)
+        {
+            string home = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            return path.Replace("~", home);
+        }
+    }
+    class PrimeiroArquivo
+    {
+        public static void Executar()
+        {
+            var path = @"~/primeiro_arquivo.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Esse é");
+                    sw.WriteLine("o nosso");
+                    sw.WriteLine("primeiro");
+                    sw.WriteLine("arquivo!");
+                }
+
+            }
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("");
+                sw.WriteLine("É possível");
+                sw.WriteLine("adicionar");
+                sw.WriteLine("mais texto!");
+            }
+        }
+
+    }
+}
