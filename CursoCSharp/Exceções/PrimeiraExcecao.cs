4af24a703a958a3f187e994927d3c7ff5f20430e
Victor <btf.victorsf0206@gmail.com>
2023-09-12 16:51:36 -0300

    Adição da classe "PrimeiraExcecao"

---
+++ CursoCSharp/Exceções/PrimeiraExcecao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Exceções
+{
+
+    public class Conta
+    {
+        double Saldo;
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+    }
+        internal class PrimeiraExcecao
+        {
+        public static void Executar()
+        {
+            var conta = new Conta(1_223.45);
+
+            try
+            {
+                conta.Sacar(1600);
+                Console.WriteLine("Retirada com Sucesso!");
+
+            } catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado!");
+            }
+        }
+    
+    }
+
+}
